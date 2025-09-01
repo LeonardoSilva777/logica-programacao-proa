e4e72cd11e024a7a4134761ffc70b9685fcb98a3
LeonardoSilva777 <leop.silva1622@gmail.com>
2025-09-01 09:14:29 -0300

    Primeira aula de JS
    Testando o JS pela primeira vez

---
+++ Aula-JS/js/teste-js.js
@@ -0,0 +1,25 @@
+function exibe_shelby() {
+    document.getElementById("shelby").style.visibility ="visible";
+    document.getElementById("pagany").style.visibility = "hidden";
+    document.getElementById("mclaren").style.visibility = "hidden";
+}
+
+function exibe_pagany() {
+    document.getElementById("shelby").style.visibility ="hidden";
+    document.getElementById("pagany").style.visibility = "visible";
+    document.getElementById("mclaren").style.visibility = "hidden";
+}
+
+function exibe_mclaren() {
+    document.getElementById("shelby").style.visibility ="hidden";
+    document.getElementById("pagany").style.visibility = "hidden";
+    document.getElementById("mclaren").style.visibility = "visible";
+}
+
+function some_tudo() {
+    document.getElementById("shelby").style.visibility ="hidden";
+    document.getElementById("pagany").style.visibility = "hidden";
+    document.getElementById("mclaren").style.visibility = "hidden";
+}
+
+alert(15+15);
